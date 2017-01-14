744b52bc21d6c248843d9c5a61146ac6358300bb
Rudy <rudy.grondin@dsinstruments.fr>
2017-01-14 18:19:47 +0100

    display the current connected user

--- imports/startup/client/client.js
+++ imports/startup/client/client.js
@@ -1,6 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import "/imports/ui/stylesheets/global.css";
 
 import "/imports/ui/layouts/main.js";
 
 Meteor.subscribe('messages');
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
